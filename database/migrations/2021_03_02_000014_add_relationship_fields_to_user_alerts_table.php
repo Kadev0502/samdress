c37a751aaf898c5f1b71572cda64bac6669e7eb7
Laravel Daily <info@laraveldaily.com>
2021-03-02 13:29:40 +0000

    QuickAdminPanel automatic commit

--- database/migrations/2021_03_02_000014_add_relationship_fields_to_user_alerts_table.php
+++ database/migrations/2021_03_02_000014_add_relationship_fields_to_user_alerts_table.php
@@ -4,13 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRelationshipFieldsToExpenseCategoriesTable extends Migration
+class AddRelationshipFieldsToUserAlertsTable extends Migration
 {
     public function up()
     {
-        Schema::table('expense_categories', function (Blueprint $table) {
+        Schema::table('user_alerts', function (Blueprint $table) {
             $table->unsignedBigInteger('created_by_id')->nullable();
-            $table->foreign('created_by_id', 'created_by_fk_3324655')->references('id')->on('users');
+            $table->foreign('created_by_id', 'created_by_fk_3324691')->references('id')->on('users');
         });
     }
 }

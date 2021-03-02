dfc62b245130cd9090684dfff84dc0b0988df2ed
Laravel Daily <info@laraveldaily.com>
2021-03-02 10:57:19 +0000

    QuickAdminPanel automatic commit

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -17,27 +17,6 @@ class UsersTableSeeder extends Seeder
                 'password'       => bcrypt('password'),
                 'remember_token' => null,
             ],
-            [
-                'id'             => 2,
-                'name'           => 'Usher',
-                'email'          => 'usher@admin.com',
-                'password'       => bcrypt('password'),
-                'remember_token' => null,
-            ],
-            [
-                'id'             => 3,
-                'name'           => 'Chana',
-                'email'          => 'chana@admin.com',
-                'password'       => bcrypt('password'),
-                'remember_token' => null,
-            ],
-            [
-                'id'             => 4,
-                'name'           => 'Assamela',
-                'email'          => 'assamela@admin.com',
-                'password'       => bcrypt('password'),
-                'remember_token' => null,
-            ],
         ];
 
         User::insert($users);

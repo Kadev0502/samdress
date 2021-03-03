f49d80292b0671844622d56609e32f2f2164dae1
Laravel Daily <info@laraveldaily.com>
2021-03-03 11:33:06 +0000

    QuickAdminPanel automatic commit

---
+++ resources/views/admin/articles/relationships/articleStocks.blade.php
@@ -0,0 +1,188 @@
+<div class="content">
+    @can('stock_create')
+        <div style="margin-bottom: 10px;" class="row">
+            <div class="col-lg-12">
+                <a class="btn btn-success" href="{{ route('admin.stocks.create') }}">
+                    {{ trans('global.add') }} {{ trans('cruds.stock.title_singular') }}
+                </a>
+            </div>
+        </div>
+    @endcan
+    <div class="row">
+        <div class="col-lg-12">
+
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    {{ trans('cruds.stock.title_singular') }} {{ trans('global.list') }}
+                </div>
+                <div class="panel-body">
+
+                    <div class="table-responsive">
+                        <table class=" table table-bordered table-striped table-hover datatable datatable-articleStocks">
+                            <thead>
+                                <tr>
+                                    <th width="10">
+
+                                    </th>
+                                    <th>
+                                        {{ trans('cruds.stock.fields.id') }}
+                                    </th>
+                                    <th>
+                                        {{ trans('cruds.stock.fields.preorder_code') }}
+                                    </th>
+                                    <th>
+                                        {{ trans('cruds.stock.fields.supplier') }}
+                                    </th>
+                                    <th>
+                                        {{ trans('cruds.stock.fields.size') }}
+                                    </th>
+                                    <th>
+                                        {{ trans('cruds.stock.fields.article') }}
+                                    </th>
+                                    <th>
+                                        {{ trans('cruds.article.fields.link') }}
+                                    </th>
+                                    <th>
+                                        {{ trans('cruds.article.fields.unit_price') }}
+                                    </th>
+                                    <th>
+                                        {{ trans('cruds.stock.fields.quantity') }}
+                                    </th>
+                                    <th>
+                                        {{ trans('cruds.stock.fields.total_price') }}
+                                    </th>
+                                    <th>
+                                        {{ trans('cruds.stock.fields.created_at') }}
+                                    </th>
+                                    <th>
+                                        {{ trans('cruds.stock.fields.updated_at') }}
+                                    </th>
+                                    <th>
+                                        &nbsp;
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($stocks as $key => $stock)
+                                    <tr data-entry-id="{{ $stock->id }}">
+                                        <td>
+
+                                        </td>
+                                        <td>
+                                            {{ $stock->id ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $stock->preorder_code ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $stock->supplier->name ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $stock->size->name ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $stock->article->name ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $stock->article->link ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $stock->article->unit_price ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $stock->quantity ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $stock->total_price ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $stock->created_at ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $stock->updated_at ?? '' }}
+                                        </td>
+                                        <td>
+                                            @can('stock_show')
+                                                <a class="btn btn-xs btn-primary" href="{{ route('admin.stocks.show', $stock->id) }}">
+                                                    {{ trans('global.view') }}
+                                                </a>
+                                            @endcan
+
+                                            @can('stock_edit')
+                                                <a class="btn btn-xs btn-info" href="{{ route('admin.stocks.edit', $stock->id) }}">
+                                                    {{ trans('global.edit') }}
+                                                </a>
+                                            @endcan
+
+                                            @can('stock_delete')
+                                                <form action="{{ route('admin.stocks.destroy', $stock->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+                                                    <input type="hidden" name="_method" value="DELETE">
+                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                                    <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
+                                                </form>
+                                            @endcan
+
+                                        </td>
+
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+@section('scripts')
+@parent
+<script>
+    $(function () {
+  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
+@can('stock_delete')
+  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
+  let deleteButton = {
+    text: deleteButtonTrans,
+    url: "{{ route('admin.stocks.massDestroy') }}",
+    className: 'btn-danger',
+    action: function (e, dt, node, config) {
+      var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
+          return $(entry).data('entry-id')
+      });
+
+      if (ids.length === 0) {
+        alert('{{ trans('global.datatables.zero_selected') }}')
+
+        return
+      }
+
+      if (confirm('{{ trans('global.areYouSure') }}')) {
+        $.ajax({
+          headers: {'x-csrf-token': _token},
+          method: 'POST',
+          url: config.url,
+          data: { ids: ids, _method: 'DELETE' }})
+          .done(function () { location.reload() })
+      }
+    }
+  }
+  dtButtons.push(deleteButton)
+@endcan
+
+  $.extend(true, $.fn.dataTable.defaults, {
+    orderCellsTop: true,
+    order: [[ 1, 'desc' ]],
+    pageLength: 100,
+  });
+  let table = $('.datatable-articleStocks:not(.ajaxTable)').DataTable({ buttons: dtButtons })
+  $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e){
+      $($.fn.dataTable.tables(true)).DataTable()
+          .columns.adjust();
+  });
+  
+})
+
+</script>
+@endsection
